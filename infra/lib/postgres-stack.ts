40795e97a111326e6c545f6e7e5ea9071b2fc42b
Hiroki SAKABE <hiroki.sakabe@icloud.com>
2023-06-04 02:00:16 +0000

    wip

--- infra/lib/postgres-stack.ts
+++ infra/lib/postgres-stack.ts
@@ -1,7 +1,8 @@
-import { Stack, StackProps, RemovalPolicy } from "aws-cdk-lib";
+import { Stack, StackProps, RemovalPolicy, CfnOutput } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as rds from "aws-cdk-lib/aws-rds";
+import * as secretsmanager from "aws-cdk-lib/aws-secretsmanager";
 
 interface Props extends StackProps {
   vpc: ec2.Vpc;
@@ -17,14 +18,32 @@ export class PostgresStack extends Stack {
       version: rds.AuroraPostgresEngineVersion.VER_13_4,
     });
 
+    const auroraClusterSecret = new secretsmanager.Secret(
+      this,
+      "AuroraClusterCredentials",
+      {
+        secretName: "AuroraClusterCredentials",
+        description: "AuroraClusterCrendetials",
+      }
+    );
+
+    // aurora credentials
+    const auroraClusterCrendentials =
+      rds.Credentials.fromSecret(auroraClusterSecret);
+
     const aurora_cluster = new rds.DatabaseCluster(this, "AuroraDatabase", {
       engine: auroraEngine,
       removalPolicy: RemovalPolicy.DESTROY,
       instanceProps: {
         vpc: props.vpc,
       },
+      credentials: auroraClusterCrendentials,
     });
 
     this.clusterEndpoint = aurora_cluster.clusterEndpoint;
+
+    new CfnOutput(this, "AuroraClusterCredentialSecretArn", {
+      value: aurora_cluster.secret?.secretArn || "",
+    });
   }
 }
